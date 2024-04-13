49cf4d7587c8c4f2b3186746ea7431deb00eeca1
Adarsh <adarsh9540984202@gmail.com>
2024-04-13 14:52:47 +0530

    add till here

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 
 import './App.css'
+// import {Btn} from './components/Button'
 import Header from './components/Header'
-
+import  Body  from './components/Body'
 function App() {
   
 
   return (
-    <>
+    <div className='bg-black min-h-screen'>
      <Header/>
-    </>
+     {/* <Btn/> */}
+     <Body/>
+    </div>
   )
 }
 
