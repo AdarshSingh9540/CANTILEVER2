c6ad4ee5c3823c7f10d6952c2db5bc2aa1f0a2ed
Adarsh <adarsh9540984202@gmail.com>
2024-04-13 15:43:48 +0530

    add news card

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -30,19 +30,51 @@ const ExpandMore = styled((props: ExpandMoreProps) => {
   }),
 }));
 
+
+interface Article {
+    source: {
+      id: string | null;
+      name: string;
+    };
+    author: string;
+    title: string;
+    description: string;
+    url: string;
+    // Add other properties if needed
+  }
+  
+  interface ApiResponse {
+    status: string;
+    totalResults: number;
+    articles: Article[];
+  }
+  
+
 export default function Body() {
   const [expanded, setExpanded] = React.useState(false);
+  const [news, setNews] = React.useState<ApiResponse | null>(null);
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
+
+  async function fetchData (){
+    const data = await fetch("https://newsapi.org/v2/everything?q=apple&from=2024-04-12&to=2024-04-12&sortBy=popularity&apiKey=bec4aae11a2d4dc2b97c5220c373f271");
+    const json = await data.json();
+    console.log(json)
+    setNews(json)
+  }
+
+React.useEffect(()=>{
+    fetchData();
+},[]);
 
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardHeader
         avatar={
           <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            R
+            {news?.articles[7]?.author.charAt(0)}
           </Avatar>
         }
         action={
@@ -50,20 +82,19 @@ export default function Body() {
             <MoreVertIcon />
           </IconButton>
         }
-        title="Shrimp and Chorizo Paella"
-        subheader="September 14, 2016"
+        // title={news?.articles[71]?.title}
+        title = {news?.articles[51]?.source?.name}
+        subheader={news?.articles[51]?.publishedAt}
       />
       <CardMedia
         component="img"
         height="194"
-        image="https://akm-img-a-in.tosshub.com/businesstoday/images/story/202312/658e5b5ebba32-xiaomi-su7-ev-to-take-on-tesla-293837105-16x9.jpg?size=948:533"
+        image={news?.articles[51]?.urlToImage}
         alt="Paella dish"
       />
       <CardContent>
         <Typography variant="body2" color="text.secondary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the mussels,
-          if you like.
+         {news?.articles[51]?.title}
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
@@ -86,28 +117,11 @@ export default function Body() {
         <CardContent>
           <Typography paragraph>Method:</Typography>
           <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-            aside for 10 minutes.
+          {news?.articles[51]?.description}
           </Typography>
+          
           <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
-            medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
-            occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
-            large plate and set aside, leaving chicken and chorizo in the pan. Add
-            pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
-            stirring often until thickened and fragrant, about 10 minutes. Add
-            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-          </Typography>
-          <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and
-            peppers, and cook without stirring, until most of the liquid is absorbed,
-            15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
-            mussels, tucking them down into the rice, and cook again without
-            stirring, until mussels have opened and rice is just tender, 5 to 7
-            minutes more. (Discard any mussels that don&apos;t open.)
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then serve.
+            {news?.articles[51]?.content}
           </Typography>
         </CardContent>
       </Collapse>
